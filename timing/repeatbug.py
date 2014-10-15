e443c17bd450d73218de40b34a50151bc61b1d65
Tony Ling <tl2573@columbia.edu>
2014-10-14 21:05:16 -0400

    added timing tests

---
+++ timing/repeatbug.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import subprocess
+
+libsafecount=0
+for i in range(1000):
+	try:
+		subprocess.check_output('./bug.sh', stderr=subprocess.STDOUT)
+	except subprocess.CalledProcessError as e:
+		if e.output.split()[0] == 'Libsafe':
+			print e.output
+			libsafecount += 1
+print str(libsafecount) + ' out of 1000 exploit attempts were caught by libsafe.'
